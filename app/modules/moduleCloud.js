3cb6bdc7cd6676045406517657073d20cf692b71
easy4docker <easy4docker@gmail.com>
2021-01-07 23:42:46 -0800

    Update moduleCloud.js

--- app/modules/moduleCloud.js
+++ app/modules/moduleCloud.js
@@ -68,7 +68,7 @@ const { exit } = require('process');
 				let cmd = 'sed /' + data.fileName.replace(/[-\/\\^$*+?.()|[\]{}_]/g, '\\$&') + '/d /etc/crontab > /etc/tmp_crontab';
 				cmd += ' && cp -f /etc/tmp_crontab /etc/crontab && rm /etc/tmp_crontab';
 
-				const fnc = env.dataFolder + '/cron/xc_' + new Date().getTime() + '.sh';
+				const fnc = _env.data_folder + '/cron/xc_' + new Date().getTime() + '.sh';
 					
 				fs.writeFile(fnc, cmd, (errp) => {
 					cbk(true);
