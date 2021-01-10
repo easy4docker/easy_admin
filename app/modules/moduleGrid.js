58216ab2c58957653e735aba744a96783aff58ec
easy4docker <easy4docker@gmail.com>
2021-01-09 23:55:44 -0800

    Update moduleGrid.js

--- app/modules/moduleGrid.js
+++ app/modules/moduleGrid.js
@@ -5,15 +5,20 @@
             CP = new pkg.crowdProcess(),
             data_dir = '/var/_localAppData',
             key_dir = '/var/_localAppKey',
-            sitesCfgFn = data_dir + '/_servers_cfg.json';
+            sitesCfgFn = data_dir + '/_servers_cfg.json',
+            keyfn = key_dir + '_grid.json';
+            
 
         me.call = () => {
             let p = req.params[0],
                 mp = p.match(/\/([^\/]+)\/([^\/]+)(\/|$)/);
+            
             if (mp) {
                 switch (mp[2])  {
                     case 'updateStatus':
-                        res.send('updateStatus');
+                        me.saveGrid(req.query.ip, () => {
+                            es.send(true);
+                        });
                         break;
                     case 'getIp':
                         fs.readFile(data_dir + '/_ip', 'utf-8', (err, data)=> {
@@ -29,6 +34,22 @@
                 res.send('wrong path');
             }
             
+        };
+
+        me.getGrid = () => {
+            let grid = {};
+            try {
+                grid = pkg.require(keyfn);
+            } catch (e) {}
+            return grid;
+        }
+
+        me.saveGrid = (ip, callback) => {
+            let grid = me.getGrid();
+            grid[id] = new Date().getTime();
+            fs.writeFile(keyfn, JSON.stringify(grid), (err) => {
+				callback();
+			});
         }
     }
     module.exports = obj;
