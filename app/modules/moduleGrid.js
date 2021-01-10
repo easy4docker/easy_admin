a6be3dd716c92caeafacf7990f0f91dfac9648bd
easy4docker <easy4docker@gmail.com>
2021-01-09 22:54:05 -0800

    update

--- app/modules/moduleGrid.js
+++ app/modules/moduleGrid.js
@@ -7,8 +7,12 @@
             key_dir = '/var/_localAppKey',
             sitesCfgFn = data_dir + '/_servers_cfg.json';
 
-        me.call = () => {
-            fs.readFile(data_dir + '/ip', 'utf-8', (err, data)=> {
+        me.call = (p) => {
+            let p = req.params[0],
+                mp = p.match(/\/([^\/]+)\/([^\/]+)(\/|$)/);
+            res.send(mp);
+            return true;
+            fs.readFile(data_dir + '/_ip', 'utf-8', (err, data)=> {
                 res.send(data);
                 
             });    
