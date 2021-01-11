f55eff04fd5689a442ef25c79c49b7ddcfaa7c5e
easy4docker <easy4docker@gmail.com>
2021-01-11 10:45:04 -0800

    update

--- app/modules/moduleGrid.js
+++ app/modules/moduleGrid.js
@@ -69,15 +69,28 @@
 
         me.addGrid = (data) => {
             const _f = {};
+
+            let gridServer = me.getGrids();
+            if (data.gridServer) {
+                gridServer[data.gridServer] = data.tag;
+            }
+
             _f['saveGrids'] = (cbk) => {
-                let gridServer = me.getGrids();
-                if (data.gridServer) {
-                    gridServer[data.gridServer] = data.tag;
-                }
+
                 fs.writeFile(gridServerFn, JSON.stringify(gridServer), (err) => {
                     cbk(true);
                 });
             };
+            _f['addToCron'] = (cbk) => {
+                const str = 'echo "* * * * *  root (echo _EASY_GRID_SYNC && cd  ${SCR_DIR} && sh _gridSync.sh) ' + 
+                    encodeURIComponent(JSON.stringify(gridServer)) + '" >> /etc/crontab ';
+                fs.writeFile(data_dir + '/commCron/gridSync_' + new Date().getTime() + '.sh', str, function (err) {
+                    setTimeout(() => {
+                        callback({status:'success', message: code});
+                    }, 500)
+                });
+                cbk(true);
+            }
             CP.serial(_f, (data) => {
                 res.send(me.getGrids());
             }, 3000)
@@ -109,6 +122,8 @@
             });
 
             /*
+              echo "* * * * *  root (echo _EASY_DOCKER && cd  ${SCR_DIR} && sh _gridSync.sh)" >> /etc/crontab
+
             _f['removeCron'] = (cbk) => {
 				let cmd = 'sed /' + data.fileName.replace(/[-\/\\^$*+?.()|[\]{}_]/g, '\\$&') + '/d /etc/crontab > /etc/tmp_crontab';
 				cmd += ' && cp -f /etc/tmp_crontab /etc/crontab && rm /etc/tmp_crontab';
