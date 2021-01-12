2eca00c9e9d476c1ad538a5ac2d8a51447ad83fd
easy4docker <easy4docker@gmail.com>
2021-01-11 23:22:57 -0800

    Update moduleGrid.js

--- app/modules/moduleGrid.js
+++ app/modules/moduleGrid.js
@@ -71,7 +71,7 @@
         me.updateStatus = (data, cbk) => {
             var grids = me.getGridStatus();
             if (data.ip) {
-                grids[data.ip] = {tm: new Date().getTime(), contents: data.contents};
+                grids[data.ip] = {tm: new Date().getTime(), contents: data.contents, v : 'v'};
                 fs.writeFile(gridStatusFn, JSON.stringify(grids), (err) => {
                     cbk(true);
                 });
