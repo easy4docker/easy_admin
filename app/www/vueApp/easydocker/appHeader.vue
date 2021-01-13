18a3d9d47b2f41e452545646d33cf7b391a36f5f
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-01-12 22:49:13 -0800

    Update appHeader.vue

--- app/www/vueApp/easydocker/appHeader.vue
+++ app/www/vueApp/easydocker/appHeader.vue
@@ -6,7 +6,7 @@
                     <div class="col-2 p-0 m-0 text-left">
                     </div>
                     <div class="col-8 p-2 m-0 text-center">
-                        <h1 class="header-title">EasyDocker Grid Admin 11</h1>
+                        <h1 class="header-title">EasyDocker Grid Admin</h1>
                     </div>
                     <div class="col-2 p-0 m-0 text-right">
                     </div>
