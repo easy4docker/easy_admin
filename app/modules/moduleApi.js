4831d6bb9feac4841bda9461fb1e2dd001a1038c
easy4docker <easy4docker@gmail.com>
2021-01-14 22:35:40 -0800

    Update moduleApi.js

--- app/modules/moduleApi.js
+++ app/modules/moduleApi.js
@@ -26,7 +26,7 @@
         };
 
         me.getGridMatrix = (data) => {
-            res.send({status: 'success', result: me.dataGridMatrix(), data: data});
+            res.send({status: 'success', result: me.dataGridMatrix()});
         }
 
         me.dataGridMatrix = () => {
@@ -37,6 +37,109 @@
             return grids;
         }
 
+        me.updateStatus = (data, cbk) => {
+            var grids = me.dataGridMatrix();
+            if (data.ip) {
+                grids[data.ip] = {tm: new Date().getTime(), token: data.token, server: data.server, tag: data.tag};
+                fs.writeFile(gridStatusFn, JSON.stringify(grids), (err) => {
+                    cbk(true);
+                });
+            } else {
+                cbk(false);
+            }
+        }
+        // <---- get related ---->
+        me.getGrids = () => {
+            res.send(me.dataGrids());
+        }
+        me.dataGrids = () => {
+            let grids = {};
+            try {
+                grids = pkg.require(gridServerFn);
+            } catch (e) {}
+            return grids;
+        }
+
+        me.makeid = (length) => {
+            var result           = '';
+            var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            var charactersLength = characters.length;
+            for ( var i = 0; i < length; i++ ) {
+               result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+         }
+
+        me.addGrid = () => {
+            var data = req.body;
+            const _f = {};
+
+            let gridServer = me.dataGrids();
+            if (data.gridServer) {
+                gridServer[data.gridServer] = data.tag;
+            }
+
+            _f['saveGrids'] = (cbk) => {
+                fs.writeFile(gridServerFn, JSON.stringify(gridServer), (err) => {
+                    cbk(true);
+                });
+            };
+            _f['addToCron'] = (cbk) => {
+                let shell_str = 'echo "*/5 * * * *  root (echo _EASY_GRID_SYNC && cd  ' + _env.app_root + ' && sh _gridSync.sh ' + 
+                    data.gridServer + ' ' + data.tag + ' ' + me.makeid(32) + ')" >> ';
+
+                if (_env.env === 'local') {
+                    shell_str += _env.data_folder + '/log/ctab';
+                } else {
+                    shell_str += '/etc/crontab';
+                }
+                me.setCron('gridSync', shell_str, (err) => {
+                    cbk(true);
+                });
+            }
+            CP.serial(_f, (data) => {
+                me.getGrids();
+            }, 3000)
+
+        }
+        //------
+        me.syncAppCode = () => {
+            const shell_str = 'cd ' + git_root + ' && git pull';
+            exec(shell_str, {maxBuffer: 1024 * 2048},
+                function(error, stdout, stderr) {
+                    res.send({status : 'success'})
+            });
+        }
+        me.removeGrid = () => {
+            var data = req.body;
+            const _f = {};
+            let gridServer = me.dataGrids();
+            _f['demoveGrid'] = (cbk) => {
+                if (data.gridServer) {
+                    delete gridServer[data.gridServer];
+                }
+                fs.writeFile(gridServerFn, JSON.stringify(gridServer), (err) => {
+                    cbk(true);
+                });
+            };
+            _f['removeCron'] = (cbk) => {
+                let shell_fn = (_env.env === 'local')? (_env.data_folder + '/log/ctab') : '/etc/crontab';
+                let shell_str = "sed '/\echo _EASY_GRID_SYNC/d' " + shell_fn + " > /tmp/crontab_easy_grid &&  cp -f /tmp/crontab_easy_grid " + shell_fn;
+                me.setCron('remove-grid', shell_str, (err) => {
+                    cbk(true);
+                });
+            }
+            
+            CP.serial(_f, (data) => {
+                me.getGrids();
+            }, 3000)
+        }
+
+        me.setCron = (code, str, callback) => {
+            fs.writeFile(data_dir + '/commCron/' + code + '_' + new Date().getTime() + '.sh', str, function (err) {
+                callback({status:'success'});
+            });
+        }
     }
     module.exports = obj;
 })()
