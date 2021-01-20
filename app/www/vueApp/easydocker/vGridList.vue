fd81973f56d05f63c3d77e1445cdf550406d7a3b
easy4docker <easy4docker@gmail.com>
2021-01-19 23:05:43 -0800

    Update vGridList.vue

--- app/www/vueApp/easydocker/vGridList.vue
+++ app/www/vueApp/easydocker/vGridList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-left  alert-secondary border rounded grids-list-section">
+    <div class="text-left p-2 alert-secondary border rounded grids-list-section">
         <div class="p-2 text-center">Grid Servers</div>
         <span v-for="(v, k) in root.gridMatrix">
             <div class="pr-3"><input type="checkbox"><span class="pl-2">{{ k }}</span></div>
