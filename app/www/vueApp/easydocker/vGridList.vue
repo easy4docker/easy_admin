877588c00b0217dcf191fb0f4bfab47c2b98d13c
easy4docker <easy4docker@gmail.com>
2021-01-19 22:36:04 -0800

    Update vGridList.vue

--- app/www/vueApp/easydocker/vGridList.vue
+++ app/www/vueApp/easydocker/vGridList.vue
@@ -1,5 +1,5 @@
 <template>
-    <span v-for="(v, k) in root.gridMatrix">
+    <span v-for="(v, k) in root.gridMatrix">===
         <div class="pr-3"><input type="checkbox" :checked="isFilterChecked(k)" v-on:click="checkFilter(k)"><span class="pl-2">{{ k }}</span></div>
     </span>
 </template>
