09124df5bf131f00a1c6ed5283c93a65b02c7688
easy4docker <easy4docker@gmail.com>
2021-01-19 22:07:14 -0800

    Update appBody.vue

--- app/www/vueApp/easydocker/appBody.vue
+++ app/www/vueApp/easydocker/appBody.vue
@@ -1,12 +1,7 @@
 <template>
     <div class="container-fluid m-0 mt-2 mb-3" >
         <div class="row">
-            <div class="col-2 p-0">--
-                <span v-for="(v, k) in root.gridMatrix">
-                    <div class="pr-3"><input type="checkbox" :checked="isFilterChecked(k)" v-on:click="checkFilter(k)"><span class="pl-2">{{ k }}</span></div>
-                </span>
-            </div>
-            <div class="col-10 p-0 card text-center shadow border rounded">
+            <div class="col-12 p-0 card text-center shadow border rounded">
                 <span v-if="root.isSignin()">
                     <v-form-server v-if="root.matrix('form')"></v-form-server>
                     <v-server-list v-if="root.matrix('list')"></v-server-list>
@@ -27,26 +22,8 @@ module.exports = {
     },
     mounted() {
         var me = this;
-        setTimeout(
-            function() {
-                me.getVServerList();
-            }, 50
-        );
     },
     methods : {
-        isFilterChecked(k) {
-            var me = this;
-            return (me.serverTypeFilter.indexOf(k) !== -1);
-        },
-        getVServerList() {
-            var me = this;
-            me.root.dataEngine().getVServerList(
-                false,
-                function(result) {
-                    me.list = result.list;
-                }
-            );
-        },
     },
     components: VUEApp.loadComponents({
         LOAD    : {
