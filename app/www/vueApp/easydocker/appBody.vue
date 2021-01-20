515471fdace836ac7df6a5d6ac2a48987e9a072f
easy4docker <easy4docker@gmail.com>
2021-01-19 21:56:52 -0800

    Update appBody.vue

--- app/www/vueApp/easydocker/appBody.vue
+++ app/www/vueApp/easydocker/appBody.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        <span v-if="root.isSignin()">
-          <v-form-server v-if="root.matrix('form')"></v-form-server>
-          <v-server-list v-if="root.matrix('list')"></v-server-list>
-          <v-form-grid v-if="root.matrix('gridSetup')"></v-form-grid>
-          <local-grid v-if="root.matrix('localGrid')"></local-grid>
-        </span>
+    <div class="container-fluid m-0 mt-2 mb-3" >
+        <div class="row">
+            <div class="col-1 p-0"></div>
+            <div class="col-11 p-0 card text-center shadow border rounded">
+                <span v-if="root.isSignin()">
+                    <v-form-server v-if="root.matrix('form')"></v-form-server>
+                    <v-server-list v-if="root.matrix('list')"></v-server-list>
+                    <v-form-grid v-if="root.matrix('gridSetup')"></v-form-grid>
+                    <local-grid v-if="root.matrix('localGrid')"></local-grid>
+                </span>
+            </div>
+        </div>   
     </div>
 </template>
  
