016413541e251b4d9725ee163dcc04d66d4ff031
easy4docker <easy4docker@gmail.com>
2021-01-19 22:57:45 -0800

    Update vServerList.vue

--- app/www/vueApp/easydocker/vServerList.vue
+++ app/www/vueApp/easydocker/vServerList.vue
@@ -18,7 +18,7 @@
         <div class="card-body card-list-section border-0 pt-0">
             <div class="container-fluid p-1 pb-0" >
                 <div class="row">
-                    <div class="col-2 p-3 m-0 text-left alert-secondary border rounded grids-list-section">
+                    <div class="col-2">
                         <grid-list></grid-list>
                     </div>
                     <div class="col-10 p-0 m-0 text-left">
