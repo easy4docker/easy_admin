af9425762e9bdec484004794367aeeb8f219b04a
easy4docker <easy4docker@gmail.com>
2021-01-20 23:57:37 -0800

    Update crowdProcess.js

--- app/www/js/crowdProcess/crowdProcess.js
+++ app/www/js/crowdProcess/crowdProcess.js
@@ -71,12 +71,7 @@
             , 1); 		
         };
     };
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = obj;
-} else {
     window.crowdProcess = function() {
         return obj; 
     }
-}
 })();
