bcb481f6c319fc82c0602dcc5009754ea2d6f268
easy4docker <easy4docker@gmail.com>
2021-01-27 20:53:21 -0800

    Update moduleCommon.js

--- app/modules/moduleCommon.js
+++ app/modules/moduleCommon.js
@@ -1,6 +1,14 @@
 (function() {
     var obj = function(env, pkg, req, res) {
-        const me = this;
+        const   me = this,
+                git_root = '/var/_localRoot',
+                app_dir = '/var/_localApp',
+                data_dir = '/var/_localAppData',
+                key_dir = '/var/_localAppKey',
+                gridStatusFn = data_dir + '/_gridMatrix.json',
+                gridServerFn = key_dir + '/_gridServers.json',
+                gridTokenFn = key_dir + '/_gridToken',
+                gridOldTokenFn = key_dir + '/_gridOldToken';
         me.sendErrorJson = (message) => {
             res.send({status:'failure', code: '404', message : message});
         }
