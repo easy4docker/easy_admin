6606f4847d7927888a6c3f56e0d4e2c61a28a39f
easy4docker <easy4docker@gmail.com>
2021-01-27 17:26:27 -0800

    Update appRouter.js

--- app/modules/appRouter.js
+++ app/modules/appRouter.js
@@ -93,7 +93,7 @@
 					case '_grid':
 						var MAGrid= pkg.require(env.root+ '/modules/moduleGrid.js');
 						let mGrid =  new MAGrid(env, pkg, req, res);
-						mGrid.call('post', false);
+						mGrid.call('post', true);
 						break; 
 
 					case '_gridHub':
