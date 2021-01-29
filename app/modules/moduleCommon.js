fd99cef273fb93a0335ec9487390f4b77cc0fbeb
easy4docker <easy4docker@gmail.com>
2021-01-29 06:45:44 -0800

    Update moduleCommon.js

--- app/modules/moduleCommon.js
+++ app/modules/moduleCommon.js
@@ -1,6 +1,25 @@
 (function() {
     var obj = function(req, res) {
         const   me = this;
+        me.inside = {
+            root : '/var/_localRoot',
+            app  : '/var/_localApp',
+            data : '/var/_localAppData',
+            key  : '/var/_localAppKey'
+        }
+
+        me.outside = {
+            root : '/var/_localRoot',
+            app  : '/var/_localApp',
+            data : '/var/_localAppData',
+            key  : '/var/_localAppKey'
+        }
+
+        me.fn = {
+            authToken   : me.inside.data + '/authToken.json',
+            gridStatus  : me.inside.data +  '/authToken.json'
+        }
+
         me.sendErrorJson = (message) => {
             res.send({status:'failure', code: '404', message : message});
         }
