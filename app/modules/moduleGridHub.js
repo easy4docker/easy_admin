4144a310d23a0efeef51e1f6a955d1ae38b52fde
easy4docker <easy4docker@gmail.com>
2021-01-29 22:26:25 -0800

    Update moduleGridHub.js

--- app/modules/moduleGridHub.js
+++ app/modules/moduleGridHub.js
@@ -42,7 +42,7 @@
                             var MAGrid= pkg.require(env.root+ '/modules/moduleGrid.js');
                             let mGrid =  new MAGrid(env, pkg, req, res);
                             const grid = mGrid.dataGridMatrix();
-                            res.send({result : grid});
+                            res.send({status:'success', result : grid});
                            // mGrid.call('post', true);
                         } catch (e) {}
                     } else {
