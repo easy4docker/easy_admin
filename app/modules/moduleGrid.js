08eb2351fca9e1e638b2d31e5526c171394501a5
easy4docker <easy4docker@gmail.com>
2021-01-29 20:52:14 -0800

    Update moduleGrid.js

--- app/modules/moduleGrid.js
+++ app/modules/moduleGrid.js
@@ -8,7 +8,7 @@
             app_dir = '/var/_localApp',
             data_dir = '/var/_localAppData',
             key_dir = '/var/_localAppKey',
-            gridStatusFn = data_dir + '/_gridMatrix.json',
+            gridStatusFn = key_dir + '/_gridMatrix.json',
             gridServerFn = key_dir + '/_gridServers.json',
             gridTokenFn = key_dir + '/_gridToken',
             gridOldTokenFn = key_dir + '/_gridOldToken';
