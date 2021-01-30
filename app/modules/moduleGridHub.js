80fb747ad31d12cfeded9aa96f54b17a9982f70d
easy4docker <easy4docker@gmail.com>
2021-01-29 20:16:43 -0800

    Update moduleGridHub.js

--- app/modules/moduleGridHub.js
+++ app/modules/moduleGridHub.js
@@ -6,15 +6,16 @@
             CP = new pkg.crowdProcess(),
             git_root = '/var/_localRoot',
             app_dir = '/var/_localApp',
-            data_dir = '/var/_localAppData'
+            data_dir = '/var/_localAppData',
+            key_dir = '/var/_localAppKey',
             gridStatusFn = data_dir + '/_gridMatrix.json',
             gridTokenFn = key_dir + '/_gridToken';
 
         var MCommon= pkg.require(env.root+ '/modules/moduleCommon.js');
         me.comm = new MCommon(req, res);
 
-        //const   gridTokenFn = me.comm.gridToken,
-         //       gridStatusFn = me.comm.gridStatus;
+        // const   gridTokenFn = me.comm.gridToken,
+        //        gridStatusFn = me.comm.gridStatus;
 
 
         var _env = {};
