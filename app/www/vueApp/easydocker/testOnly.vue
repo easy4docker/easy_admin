7cea25fce76d5fda6578a0af8a002dc9a38511ee
easy4docker <easy4docker@gmail.com>
2021-02-01 09:15:58 -0800

    Update testOnly.vue

--- app/www/vueApp/easydocker/testOnly.vue
+++ app/www/vueApp/easydocker/testOnly.vue
@@ -6,8 +6,23 @@
                     <h3 class="ml-4 text-capitalize">
                         test Only
                     </h3>
+
                 </div>
                 <div class="col-7 p-3 m-0 text-right">
+                    <button class="m-2 btn btn-warning" v-on:click="setTestModule('testGridHub')">testGridHub</button>
+                    <button class="m-2 btn btn-warning" v-on:click="setTestModule('testGrid')">testGrid</button>
+                    <button class="m-2 btn btn-warning" v-on:click="setTestModule('testApi')">testApi</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 p-3 m-0 text-left">
+                    <hr/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 p-3 text-left">
+                    <div class="m-3">{{'** ' + testModule + ' **'}}</div>
+                    <div class="m-3">{{testData}}</div>
                 </div>
             </div>
         </div> 
@@ -20,230 +35,58 @@ module.exports = {
     data: function() {
         let me = this;
         return {
-            list : [],
-            serverTypes : {
-                'database'  : 'Mysql Database',
-                'databaseCloud'  : 'Mysql Cloud',
-                'backendCloud'   : 'Backend Cloud',
-                'webServer' : 'Web Server'
-            },
-            serverTypeFilter : [],
             root :  this.$parent.root,
-            currentServer : '',
-            gridMatrix: {}
+            testModule  : '',
+            testData    : {}
         }
     },
     mounted() {
         var me = this;
-        me.serverTypeFilter = Object.keys(me.serverTypes);
-        setTimeout(
-            function() {
-                me.getVServerList();
-            }, 50
-        );
     },
     watch: {
-        serverTypeFilter: function(val) {
+        testModule: function() {
           var me = this;
         }
     },
     methods : {
-        isFilterChecked(k) {
-            var me = this;
-            return (me.serverTypeFilter.indexOf(k) !== -1);
+        setTestModule(v) {
+            const me = this;
+            me.testModule = v;
+            me.clallGridMatrix();
         },
-        checkFilter(k) {
-            var me = this;
-            var list = [];
-            if (me.serverTypeFilter.indexOf(k) == -1) {
-                me.serverTypeFilter.push(k);
-            } else {
-                for (var i = 0; i < me.serverTypeFilter.length; i++) {
-                    if (me.serverTypeFilter[i] != k) {
-                        list.push(me.serverTypeFilter[i]);
-                    }
-                }
-                me.serverTypeFilter = list;
+        clallGridMatrix() {
+            const me = this;             
+            let svr = localStorage.getItem('easydockerSVR'),
+                token = localStorage.getItem('easydockerTOKEN');
+            
+            svr = (!svr) ? '' :  svr.replace(/\_/g, '.');
+        
+            if (!svr || !token) {
+                return true;
             }
-            console.log(k);
-            console.log(me.serverTypeFilter);
-        },
-        filteredResult() {
-            var me = this;
-            return me.list.filter(function(item) {
-                return (me.serverTypeFilter.indexOf(item.serverType) !== -1)
+            
+            me.root.dataEngine().gridHub({
+                server  : svr,
+                cmd     :'getGridMatrix',
+                data    : {},
+                dataType: 'json',
+                gridToken   : token
+            },
+            function(result) {
+                if (result.status === 'success') {
+                    me.testData = result.result;
+                } else {
+                    me.testData = null;
+                }
+                me.$forceUpdate();
+            }, function(err) {
+                me.testData = null;
+                console.log(err);
             });
-        },
-        getVServerList() {
-            var me = this;
-            me.root.dataEngine().getVServerList(
-                false,
-                function(result) {
-                    me.list = result.list;
-                }
-            );
-        },
-        deleteVirtualServer(record) {
-            var me = this;
-            me.root.popUp(me).show({
-                insideModule: 'confirmDelete',
-                insideModuleUrl: '/vueApp/easydocker/popUpModals/confirmDelete.vue',
-                data : {
-                    serverName : record.name,
-                    serverType : record.serverType
-                }
-            });
-        },
-        switchBranch(record) {
-            let me = this,
-                data = {
-                    serverType  : record.serverType,
-                    serverName  : record.name,
-                    branch      : record.branch
-                };
-
-            me.root.popUp(me).show({
-                insideModule: 'switchBranch',
-                insideModuleUrl: '/vueApp/easydocker/popUpModals/switchBranch.vue',
-                data : data
-            });            
-        },
-        stopVServer(record) {
-            var me = this;
-            me.root.dataEngine().stopVServer(record);
-        },
-        pullCode(record) {
-            var me = this;
-            me.root.dataEngine().pullCode(record);
-        },    
-        startVServer(record) {
-            var me = this;
-            me.root.dataEngine().startVServer(record);
-        },
-        arrayPorts(item) {
-            var me = this;
-            var arr = [];
-            var cloudP = (!item || !item.docker || !item.docker.cloudPort) ? null : item.docker.cloudPort;
-            var p = (!item || !item.docker || !item.docker.ports) ? [] : item.docker.ports;
-            if (cloudP) {
-                for (var i = 0; i < cloudP.length; i++) {
-                    arr.push(30000 + (item.unidx * 1000 + parseInt(cloudP[i])))
-                }
-            } else {
-                for (var i = 0; i < p.length; i++) {
-                    arr.push(10000 + (item.unidx * 1000 + parseInt(p[i])))
-                }
-            }
-            return arr;
-        },
-        outerPorts(item) {
-            var me = this;
-            return me.arrayPorts(item).join(',');
-        },
-        saveEditorContent(record, v, callback) {
-            var me = this;
-            var rec = {
-                serverName : record.name,
-                serverType : record.serverType,
-                contents   : v
-            };
-            me.root.dataEngine().saveVserverValiables(rec, 
-                function(result) {
-                    callback(result);
-            });
-        },
-        getEditorContent(record, callback) {
-            var me = this;
-            var rec = {
-                serverName : record.name,
-                serverType : record.serverType
-            };
-            me.root.dataEngine().getVserverValiables(rec, 
-                function(result) {
-                    callback(result);
-            });
-        },
-        askToken(item, callback) {
-            var me = this;
-            me.root.dataEngine().askToken(
-                item,
-                function(result) {
-                    callback(result);
-                }
-            );
-        },
-        isCloudTool(item) {
-            var me = this;
-            return  (['databaseCloud', 'backendCloud'].indexOf(item.serverType) === -1) ?  false : true;
-        },
-        linkCloudTo(item) {
-            var me = this;
-            var port = me.arrayPorts(item)
-            me.askToken(item, function(data) {
-                var tokenlist = (!data || !data.tokens || !data.tokens.list) ? {} : data.tokens.list;
-                window.open('/cloudTools/cloudAdmin.ect?host=' + item.name + ((!Object.keys(tokenlist).length) ? '' : ('&token=' + Object.keys(tokenlist)[0])));
-            });
-        },
-        popupCloudTool(item) {
-            var me = this;
-            var port = me.arrayPorts(item);
-            me.root.popUp(me).show({
-                insideModule: 'iframeObj',
-                insideModuleUrl: '/vueApp/easydocker/popUpModals/iframeObj.vue',
-                data : {
-                    url : '/html/tools/cloudTokenAdmin.ect?port=' + port + '&serverName=' + item.name + '&serverType=' + item.serverType
-                },
-                noDefaultCancel : true
-            }); 
-
-            document._iFrameBridge.close = (function(me) {
-                return function(v) {
-                    me.root.popUp(me).close();
-                }
-            })(me);
-        },
-        popupEditor(record) {
-            var me = this;
-            me.root.popUp(me).show({
-                insideModule: 'iframeObj',
-                insideModuleUrl: '/vueApp/easydocker/popUpModals/iframeObj.vue',
-                data : {
-                    url : '/html/tools/aceEditor.ect?mode=json',
-                    item : record
-                },
-                noDefaultCancel : true
-            }); 
-
-            document._iFrameBridge.close = (function(me) {
-                return function(v) {
-                    me.root.popUp(me).close();
-                }
-            })(me);
-
-            document._iFrameBridge.save = (function(me, item) {
-                return function(v) {
-                   me.saveEditorContent(item, v, function(result) {
-                       me.root.popUp(me).close();
-                   })
-                }
-            })(me, record);
-
-            document._iFrameBridge.loadContents = (function(me, item) {
-                return function(callback) {
-                   me.getEditorContent(item, function(result) {
-                       callback(result.data);
-                   })
-                }
-            })(me, record);
         }
     },
     components: VUEApp.loadComponents({
-        LOAD    : {
-            'selectBranch'   : '/vueApp/easydocker/selectBranch.vue',
-            'dockerAdupter'  : '/vueApp/easydocker/dockerAdupter.vue',
-            'iframeObj'      : '/vueApp/easydocker/_iframe.vue',
-            'gridList'      : '/vueApp/easydocker/vGridList.vue'
-        }, 
+        LOAD    : {}, 
         TPL :{}
     })
 }
