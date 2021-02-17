a5c7cdc113ffc82e58d88f5d6e54159960293d55
easy4docker <easy4docker@gmail.com>
2021-02-17 13:21:25 -0800

    Update testOnly.vue

--- app/www/vueApp/easydocker/testOnly.vue
+++ app/www/vueApp/easydocker/testOnly.vue
@@ -22,7 +22,7 @@
             </div>
             <div class="row">
                 <div class="col-2 p-3 text-left">
-                    Test through grid -- {{Object.keys(root.gridMatrix).length}}
+                    Through grid -- {{Object.keys(root.gridMatrix).length}}
                     <hr v-if="(root.gridMatrix) &&  Object.keys(root.gridMatrix).length"/>
                     <span v-for="(v, k) in root.gridMatrix">
                         <div class="pr-3">
@@ -39,7 +39,7 @@
                     </span>
                 </div>
                 <div class="col-2 p-3 text-left">
-                    Test through gridHub -- {{Object.keys(root.gridMatrix).length}}
+                    Through gridHub -- {{Object.keys(root.gridMatrix).length}}
                     <hr v-if="(root.gridMatrix) &&  Object.keys(root.gridMatrix).length"/>
                     <span v-for="(v, k) in root.gridMatrix">
                         <div class="pr-3">
