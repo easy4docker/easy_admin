89c6ae10eaeddc6c52ae60684ae54a133eb9b9ab
easy4docker <easy4docker@gmail.com>
2021-02-18 21:50:14 -0800

    Update moduleGridHub.js

--- app/modules/moduleGridHub.js
+++ app/modules/moduleGridHub.js
@@ -55,7 +55,6 @@
                                     res.send(body);
                                 } 
                             }
-
                         });
                     }
                 }
@@ -66,7 +65,6 @@
             var setting = req.body;
             if (!bypassAuth) {
                 const authfn =  me.comm.file.authData;
-                
                 pkg.readJson(authfn, (auth) => {
                     fs.readFile(gridTokenFn, 'utf-8', (err, gridToken) => {
                         if (!setting || !setting.gridToken || (setting.gridToken != gridToken && auth.root !== setting.gridToken)) {
@@ -79,55 +77,6 @@
             } else {
                 me.execGridPost(setting.data, ip0);
             }
-        };
-        me.validationPostBK = (ip0) => {
-            const authfn =  me.comm.file.authData;
-            pkg.readJson(authfn, (auth) => {
-                fs.readFile(gridTokenFn, 'utf-8', (err, gridToken) => {
-                    var setting = req.body;
-                   if (!setting || !setting.gridToken || (setting.gridToken != gridToken && auth.root !== setting.gridToken)) {
-                       me.comm.sendAction('', 'Unauthorized gridToken!');
-                   } else {
-                        const request = require('request');
-                        var MAGrid= pkg.require(env.root+ '/modules/moduleGrid.js');
-                        let mGrid =  new MAGrid(env, pkg, req, res);
-
-
-                        mGrid.dataGridMatrix((grid) =>{
-                            setting.target = (setting.target) ? setting.target : ip0;
-                            if  (setting.cmd === 'getGridMatrix' || setting.cmd === 'getServerToken') {
-                                mGrid.call('post', true);
-                            } else {
-                              
-                                var postData =  setting; 
-                                var channel = (!setting.channel) ? '_grid' : setting.channel;
-                                let url = 'http://' + setting.target + ':10000/' + channel + '/';
-
-                                if (setting.target) {
-                                    postData.gridToken = (grid[setting.target]) ? grid[setting.target].gridToken : '';
-                                } 
-                                if (!setting.target || !postData.gridToken) {
-                                    res.send({status : 'failure', message : 'missing target'});
-                                } else {
-                                    request.post({url: url, form: postData}, function(err,httpResponse,body){    
-                                        if (err) {
-                                            res.send({status : 'failure', message : err.message});
-                                        } else {
-                                            if (setting.type === 'json') {
-                                                res.send(body);
-                                            } else {
-                                                res.send(body);
-                                            } 
-                                        }
-    
-                                    });
-                                }
-                            }
-                        });
-                   }
-                });
-            });
-            return  true;
         };
     }
     module.exports = obj;
