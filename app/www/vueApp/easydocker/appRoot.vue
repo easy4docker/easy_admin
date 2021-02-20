bb2b3bd46bbcc68b2858e61b3c1d29824ae17e28
easy4docker <easy4docker@gmail.com>
2021-02-19 23:34:38 -0800

    Update appRoot.vue

--- app/www/vueApp/easydocker/appRoot.vue
+++ app/www/vueApp/easydocker/appRoot.vue
@@ -103,7 +103,6 @@ module.exports = {
                 data    : {}
             },
             function(result) {
-                console.log(result);
                 if (result.status === 'success') {
                     me.gridMatrix = result.result;
                 }
