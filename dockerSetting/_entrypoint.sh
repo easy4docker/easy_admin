e62e94e3ca0bcad2cccdefef96c1b18428e0e0be
easy4docker <easy4docker@gmail.com>
2021-02-21 20:50:03 -0800

    update

--- dockerSetting/_entrypoint.sh
+++ dockerSetting/_entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+sed '/\echo _EASY_MIN_CRON/d' /etc/crontab  > /tmp/crontab_easy_min_cron
+cp -f /tmp/crontab_easy_min_cron  /etc/crontab
+rm -fr /tmp/crontab_easy_min_cron
+echo "* * * * * root (echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  >> /var/_localAppData/log/minCron.log)" >> /etc/crontab
+
 cd /var/_localApp
 node server.js >> /var/_localAppData/log/console_out.txt
