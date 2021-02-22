ee58a504a52609bd2da0347398c56ba42d731c1f
easy4docker <easy4docker@gmail.com>
2021-02-21 21:26:18 -0800

    update

--- app/cronjob/minute_cron.js
+++ app/cronjob/minute_cron.js
@@ -1,2 +1,19 @@
+var fs = require('fs');
+var env = {
+    root : __dirname,
+    dataFolder : '/var/_localAppData',
+    appFolder : '/var/_localApp'
+}
 console.log(new Date());
-// free -m
+console.log(env)
+/*
+
+for f in "${SITES_PATH}"/*; do
+  if [ -f "${f}/data/commCron/REMOVE.ME" ]; then
+      mv -f ${f}/data/commCron/REMOVE.ME ${CRON_PATH}/$(date +"%H%M%S")_REMOVE.ME
+  else
+    exit 1
+  fi
+done
+
+*/
--- dockerSetting/_entrypoint.sh
+++ dockerSetting/_entrypoint.sh
@@ -2,12 +2,12 @@
 sed '/\echo _EASY_MIN_CRON/d' /etc/crontab  > /tmp/crontab_easy_min_cron
 cp -f /tmp/crontab_easy_min_cron  /etc/crontab
 rm -fr /tmp/crontab_easy_min_cron
-echo "* * * * * root (echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  >> /var/_localAppData/log/minCron.txt)" >> /etc/crontab
-echo "* * * * * root (sleep 10 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  >> /var/_localAppData/log/minCron.txt)" >> /etc/crontab
-echo "* * * * * root (sleep 20 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  >> /var/_localAppData/log/minCron.txt)" >> /etc/crontab
-echo "* * * * * root (sleep 30 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  >> /var/_localAppData/log/minCron.txt)" >> /etc/crontab
-echo "* * * * * root (sleep 40 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  >> /var/_localAppData/log/minCron.txt)" >> /etc/crontab
-echo "* * * * * root (sleep 50 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  >> /var/_localAppData/log/minCron.txt)" >> /etc/crontab
+echo "* * * * * root (echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  > /var/_localAppData/log/minCron.txt)" >> /etc/crontab
+echo "* * * * * root (sleep 10 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  > /var/_localAppData/log/minCron.txt)" >> /etc/crontab
+echo "* * * * * root (sleep 20 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  > /var/_localAppData/log/minCron.txt)" >> /etc/crontab
+echo "* * * * * root (sleep 30 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  > /var/_localAppData/log/minCron.txt)" >> /etc/crontab
+echo "* * * * * root (sleep 40 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  > /var/_localAppData/log/minCron.txt)" >> /etc/crontab
+echo "* * * * * root (sleep 50 && echo _EASY_MIN_CRON && cd /var/_localApp/cronjob/ && node minute_cron.js  > /var/_localAppData/log/minCron.txt)" >> /etc/crontab
 
 cd /var/_localApp
 node server.js >> /var/_localAppData/log/console_out.txt
