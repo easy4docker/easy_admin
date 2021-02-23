106ab7d914528059c7bcc69f0f960f68bb24a857
easy4docker <easy4docker@gmail.com>
2021-02-22 20:23:51 -0800

    Update minute_cron.js

--- app/cronjob/minute_cron.js
+++ app/cronjob/minute_cron.js
@@ -3,7 +3,6 @@ const env = {
         dataFolder : '/var/_localAppData',
         appFolder : '/var/_localApp'
       },
-      
       fs = require('fs'),
       CP = require(env.appFolder + '/vendor/crowdProcess/crowdProcess.js');
 
@@ -12,27 +11,41 @@ const MServer = require(__dirname + '/modules/moduleServer.js');
 
 fs.readdir(env.dataFolder + '/sites', (err, list) => {
   const _f = {};
-  for (var i =0; i < list.length; i++) {
+  const cp = new CP();
+  const flist =  [];
+  for (var i =0; i < list.length; i++) {    
     _f['s_' + i] = ((i) => {
       return (cbk) => {
         let dirn = env.dataFolder + '/sites/' + list[i] + '/data/commCron/';
         fs.readdir(dirn, (err1, files) => {
           if (!err1) {
-            for (var j = 0; j < files.length; j++) {
-              var mserver = new MServer(env);
-              mserver.onDemand(list[i], dirn + files[j], (data) => {
-                cbk(data);
-              });
-            }
-          }  else {
-            cbk(false);
+            for (let o in files)  flist.push(files[o]);
           }
+          cbk(true);
         });
       }
     })(i);
+
   }
-  const cp = new CP();
   cp.serial(_f, (data) => {
-    // console.log(data);
+    callApp(list[i], flist);
   }, 3000);
 });
+
+const callApp = (server, flist) => {
+  const _f = {};
+  const cp = new CP();
+  for (let o in flist) {
+    _f['s_' + o] = (()=>{
+      return (cbk) => {
+        const mserver = new MServer(env);
+        mserver.onDemand(server, flist[o], (data) => {
+          cbk(data);
+        });
+      }
+    })(o)
+  }
+  cp.serial(_f, (data) => {
+    console.log(data);
+  });
+}
