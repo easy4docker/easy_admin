76f93565d302d170bef902c96a764b1dbc19df72
easy4docker <easy4docker@gmail.com>
2021-02-23 00:14:22 -0800

    Update moduleServer.js

--- app/cronjob/modules/moduleServer.js
+++ app/cronjob/modules/moduleServer.js
@@ -3,7 +3,8 @@
         var me = this,
             fs = require('fs'),
             exec = require('child_process').exec,
-            CP = require(env.appFolder + '/vendor/crowdProcess/crowdProcess.js');
+            CP = require(env.appFolder + '/vendor/crowdProcess/crowdProcess.js'),
+            ECT = require('ect');
 
         me.onDemand = (server, file, cbk) => {
             var fn = env.dataFolder + '/sites/' + server + '/data/commCron/' + file;
@@ -18,23 +19,24 @@
    
         me.removeMe = (server, param, callback) => {
             const _f = {};
+            _f['cleanDockerContainer'] = (cbk)=> {
+                me.templateContent(server, 'removeDockerApp.tpl', (content) => {
+                    me.setCron('removeOnDemainDocker-' + server, content, cbk);
+                });
+            }
             _f['removeCfg'] = (cbk) => {
                 me.deleteServer(server, (data) => {
-                    console.log('==deleteServer==>>' + server);
-                    console.log(data);
-                    cbk(true);
+                    me.saveSites(data, () => {
+                        cbk(true);
+                    });
                 });
             }
             _f['deleteFolder'] = (cbk)=> {
                 var cmd = 'rm -fr ' + env.dataFolder + '/sites/' + server;
-                console.log(cmd);
-                cbk(cmd);
-                /*   
-                    exec(cmd, {maxBuffer: 224 * 2048},
-                        function(error, stdout, stderr) {
-                            cbk(true);
-                    });
-                */ 
+                exec(cmd, {maxBuffer: 224 * 2048},
+                    function(error, stdout, stderr) {
+                        cbk(true);
+                });
             }
 
             const cp = new CP();
@@ -43,6 +45,41 @@
                 callback(true);
             }, 3000);  
         }
+        /*------*/
+
+
+        me.siteContainer = (serverName) => {
+            return ('sites-' + serverName + '-container').toLowerCase();
+        }
+
+        me.getImageName = (serverName) => {
+            return ('sites-' + serverName + '-image').toLowerCase();
+        }
+        /*----*/
+
+        me.dockerConfig = (server, callback) => {
+            callback({
+                serverName          : server,
+                siteImage           : me.getImageName(server),
+                siteContainer       : me.siteContainer(server)
+            });
+        };
+
+        me.templateContent = (server, tplName, callback) => {
+            me.dockerConfig(server, (configJson) => {
+                
+                let cmd = '';
+                try {
+                    const tpl = ECT({ watch: true, cache: false, root: env.dataFolder + '/sites/' + server + '/code/dockerSetting/scriptTemplate/', ext : '.tpl' });
+                    cmd = tpl.render(tplName, configJson);
+                } catch(e) {
+                    cmd = 'echo "' + e.message + '"' + "\n";
+                }
+                
+                callback(cmd);
+            })
+        }
+
         me.getSites = (cbk) => {
             me.readJson(env.dataFolder + '/_servers_cfg.json', (list) => {
                 cbk(list);
