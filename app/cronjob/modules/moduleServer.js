8e5f8b1b47c38fd57f722fc917966f10ff9982e2
easy4docker <easy4docker@gmail.com>
2021-02-23 23:28:05 -0800

    Update moduleServer.js

--- app/cronjob/modules/moduleServer.js
+++ app/cronjob/modules/moduleServer.js
@@ -125,92 +125,25 @@ const { eventNames } = require('process');
         }
 
         me.addOndemand = (github, param, callback) => {
-            /* */
-            const _f = {};
-            _f['cleanDockerContainer'] = (cbk)=> {
-                me.templateContent(server, 'removeDockerApp.tpl', (content) => {
-                    me.setCron('removeOnDemainDocker-' + server, content, cbk);
-                });
-            }
-            _f['removeCfg'] = (cbk) => {
-                me.deleteServer(server, (data) => {
-                    me.saveSites(data, () => {
-                        cbk(true);
-                    });
-                });
-            }
-            _f['deleteFolder'] = (cbk)=> {
-                var cmd = 'rm -fr ' + env.dataFolder + '/sites/' + server;
+            me.serverStatus((sts)=> {
+                const postData = "'" + JSON.stringify({
+                    cmd:'deleteVServer',
+                    data : {serverName : server},
+                    authToken: sts.authToken
+                }) + "'";
+                var cmd = 'curl -d ' + postData +
+                    '  -H "Content-Type: application/json" -X POST localhost/api/';
                 exec(cmd, {maxBuffer: 224 * 2048},
                     function(error, stdout, stderr) {
-                        cbk(true);
+                        var jdata = {};
+                        try {
+                        jdata = JSON.parse(stdout);
+                        } catch (e) {}
+                        console.log(jdata);
                 });
-            }
-
-            const cp = new CP();
-            cp.serial(_f, (data) => {
-                console.log(data);
-                callback(true);
-            }, 3000);  
+            });  
         }
-        /*------*/
-
-        me.siteContainer = (serverName) => {
-            return ('sites-' + serverName + '-container').toLowerCase();
-        }
-
-        me.getImageName = (serverName) => {
-            return ('sites-' + serverName + '-image').toLowerCase();
-        }
-        /*----*/
-
-        me.dockerConfig = (server, callback) => {
-            callback({
-                serverName          : server,
-                siteImage           : me.getImageName(server),
-                siteContainer       : me.siteContainer(server)
-            });
-        };
-
-        me.templateContent = (server, tplName, callback) => {
-            me.dockerConfig(server, (configJson) => {
-                let cmd = '';
-                try {
-                    const tpl = ECT({ watch: true, cache: false, root: env.dataFolder + '/sites/' + server + '/code/dockerSetting/scriptTemplate/', ext : '.tpl' });
-                    cmd = tpl.render(tplName, configJson);
-                } catch(e) {
-                    cmd = 'echo "' + e.message + '"' + "\n";
-                }
-                
-                callback(cmd);
-            })
-        }
-
-        me.getSites = (cbk) => {
-            me.readJson(env.dataFolder + '/_servers_cfg.json', (list) => {
-                cbk(list);
-            });
-        }
-
-        me.deleteServer = (server, cbk) => {
-            me.getSites((sites) => {
-                delete sites[server];
-                cbk(sites);
-            });
-        }
-
-        me.saveSites = (list, callback) => {
-            fs.writeFile(env.dataFolder + '/_servers_cfg.json', JSON.stringify(list), (err) => {
-                callback(list);
-            });
-        }
-        me.setCron = (code, str, callback) => {
-            fs.writeFile(env.dataFolder + '/commCron/' + code + '_' + new Date().getTime() + '.sh', str, function (err) {
-                setTimeout(() => {
-                    callback({status:'success', message: code});
-                }, 500)
-            });
-        }
+        
         me.readJson = (path, cb) => {
             fs.readFile(path, 'utf-8', (err, data) => {
                 if (err) {
