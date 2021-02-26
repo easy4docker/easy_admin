5cf9660c11c9664388adb8d02a4a5d0dffab1690
easy4docker <easy4docker@gmail.com>
2021-02-25 21:46:26 -0800

    update

---
+++ dockerSetting/shellScript/execPassthrough.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APP_DIR="/var/_localApp"
+DATA_DIR="/var/_localAppData"
+markfile=${DATA_DIR}/siteCommCronMark.txt
+
+# --- clean longer time task -----
+for file in $(find $markfile -not -newermt '-120 seconds' 2>&1) ;do
+  if [ -f "$markfile" ]; then
+    vfn=$(<$markfile)
+    cmda="rm -fr $vfn && pkill -f $vfn > /dev/null && rm -fr $markfile >/dev/null 2>&1"
+    eval "$cmda"
+  fi
+done
+
+cd ${DATA_DIR}/cronjob/ && node minute_cron.js  > ${DATA_DIR}/log/minCron.txt
---
+++ dockerSetting/shellScript/mainService.sh
@@ -0,0 +1,15 @@
+APP_DIR="/var/_localApp"
+DATA_DIR="/var/_localAppData"
+
+cd ${APP_DIR}
+node server.js >> ${DATA_DIR}/log/console_out.txt
+
+stsCron=1
+until [ $stsCron = 0 ]
+do 
+    if [ $stsCron != 0 ] ; then
+        cd /var/shellScript
+        sh execPassthrough.sh
+    fi
+    sleep 1
+done
