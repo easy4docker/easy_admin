5b2a373066e83a81cc8cd378e425993a683c34a8
easy4docker <easy4docker@gmail.com>
2021-02-25 23:11:50 -0800

    Update moduleServer.js

--- app/cronjob/modules/moduleServer.js
+++ app/cronjob/modules/moduleServer.js
@@ -20,8 +20,9 @@ const { eventNames } = require('process');
                                 me[data.code](server, data.param, cbk);
                             } else {
                                 me.removeMark(() => {
-                                    console.log('Skipped this file ...'  + file);
-                                });
+                                    console.log('clean siteCommCronMark =>' + me.siteCommCronFn);
+                                })
+                                
                             }
                         });
                     });
@@ -31,6 +32,18 @@ const { eventNames } = require('process');
                     });
                 }
             });
+            /*
+            var fn = env.dataFolder + '/sites/' + server + '/data/commCron/' + file;
+            me.readJson(fn, (data) => {
+                exec('rm -fr ' + fn, {maxBuffer: 224 * 2048},
+                    function(error, stdout, stderr) {
+                        if (typeof me[data.code]) {
+                            me[data.code](server, data.param, cbk);
+                        } else {
+                            console.log('Missing method ' + data.code + '!');
+                        }
+                });
+            });*/
         }
         me.removeMark = (cbk) => {
             exec('rm -fr ' + me.siteCommCronFn + ' && rm -fr ' + me.siteCommCronMark, {maxBuffer: 224 * 2048}, (error, stdout, stderr) => {
