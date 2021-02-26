1089f42ecdabeb9e0c73f154fa92f8c35a772f7b
easy4docker <easy4docker@gmail.com>
2021-02-25 23:57:13 -0800

    Update moduleServer.js

--- app/modules/moduleServer.js
+++ app/modules/moduleServer.js
@@ -500,7 +500,7 @@
             });
         };
 
-        me.templateContent = (serverName, tplName, callback) => {
+        me.templateContentBK = (serverName, tplName, callback) => {
             me.dockerConfig(serverName, (configJson) => {
                 let cmd = '';
                 try {
@@ -510,6 +510,22 @@
                     cmd = 'echo "' + e.message + '"' + "\n";
                 }
                 callback(cmd);
+            })
+        }
+
+        me.templateContent = (serverName, tplName, callback) => {
+            me.dockerConfig(serverName, (configJson) => {
+                let tplFn = me.siteCodePath(serverName) + '/dockerSetting/scriptTemplate/' + tplName;
+                fs.readFile(tplFn, 'utf-8', (err, page) => {
+                        let cmd = '';
+                        if (!err) {
+                            const tpl = pkg.ECT({ root : {page: page}});
+                            cmd = tpl.render(tplName, configJson);
+                        } else {
+                            cmd = 'echo "' + err.message + '"' + "\n";
+                        }
+                        callback(cmd);
+                });
             })
         }
 
