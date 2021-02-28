9bd3a3769d03a809b7ce7c2a6557f3f0319f7652
easy4docker <easy4docker@gmail.com>
2021-02-27 21:08:10 -0800

    Update moduleServer.js

--- app/modules/moduleServer.js
+++ app/modules/moduleServer.js
@@ -479,7 +479,7 @@
                 me.asycKeyJson(serverName, ['getInitToken', 'getKeyCode'], (data) => {
                     callback({
                         serverName          : serverName,
-                        shareFolder         : me.siteShareFolder(serverName) + '_' + new Date().getTime(),
+                        shareFolder         : me.siteShareFolder(serverName),
                         dockerCodePath      : me.dockerCodePath(serverName),
                         dockerSettingPath   : me.dockerCodePath(serverName) + '/dockerSetting',
                         dockerDataPath      : me.dockerDataPath(serverName),
