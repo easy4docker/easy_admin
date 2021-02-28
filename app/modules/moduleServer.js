23e15be4c52220b07b438af79eb1022775e8f417
easy4docker <easy4docker@gmail.com>
2021-02-28 07:01:20 -0800

    Update moduleServer.js

--- app/modules/moduleServer.js
+++ app/modules/moduleServer.js
@@ -44,7 +44,9 @@
         }
 
         me.siteShareFolder = (serverName) => {
-            const folderName = (!req.body.data || !req.body.data.superPower) ?  serverName : req.body.data.superPower;
+            const serverCode = serverName.replace(/\_([0-9]+)$/, '');
+            const folderName = (!req.body.data || !req.body.data.superPower) ?  serverName: 
+                req.body.data.superPower + '/' + serverCode + '_' + new Date().getTime();
             return _env.data_folder + '/sitesShareFolder/' + folderName;
         }
 
@@ -479,7 +481,7 @@
                 me.asycKeyJson(serverName, ['getInitToken', 'getKeyCode'], (data) => {
                     callback({
                         serverName          : serverName,
-                        shareFolder         : me.siteShareFolder(serverName) + '_' + new Date().getTime(),
+                        shareFolder         : me.siteShareFolder(serverName),
                         dockerCodePath      : me.dockerCodePath(serverName),
                         dockerSettingPath   : me.dockerCodePath(serverName) + '/dockerSetting',
                         dockerDataPath      : me.dockerDataPath(serverName),
