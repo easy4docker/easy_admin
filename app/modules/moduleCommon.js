003361ab3fa74626ace9b3c8b51af337c23e73e0
easy4docker <easy4docker@gmail.com>
2021-03-01 16:05:12 -0800

    update

--- app/modules/moduleCommon.js
+++ app/modules/moduleCommon.js
@@ -17,8 +17,8 @@
         me.file = {
             authData    : me.inside.key + '/authData.json',
             authToken   : me.inside.key + '/authToken.json',
-            gridStatus  : me.inside.key +  '/_gridMatrix.json',
-            gridServer  : me.inside.key +  '/_gridServers.json',
+            gridStatus  : me.inside.key + '/_gridMatrix.json',
+            gridServer  : me.inside.key + '/_gridServers.json',
             gridToken   : me.inside.key + '/_gridToken',
             gridOldToken: me.inside.key + '/_gridOldToken'
         }
--- app/modules/moduleUpload.js
+++ app/modules/moduleUpload.js
@@ -1,16 +1,24 @@
+const { cpuUsage } = require('process');
+
 (function() {
     var obj = function(env, pkg, req, res) {
         var me = this,
             fs = require('fs'),
-            exec = require('child_process').exec;
-
+            exec = require('child_process').exec,
+            CP = new pkg.crowdProcess();
         var _env = {};
         try {
             _env = require(data_dir + '/_env.json');
         } catch (e) {}
 
         me.runUpload = () => {
-            res.send(req.files);
+            const dirn = req.body.objPath;
+            exec('mkdir -p ' + dirn, () =>{
+                exec('mv ' + req.files[0].path + ' ' + dirn, () =>{
+                    res.send(req.files);
+                });
+                
+            })
         }
     }
     module.exports = obj;
