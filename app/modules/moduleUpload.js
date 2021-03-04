28e3eb15e00af5188ac1a486bcea537e374eecb5
easy4docker <easy4docker@gmail.com>
2021-03-03 22:36:29 -0800

    Update moduleUpload.js

--- app/modules/moduleUpload.js
+++ app/modules/moduleUpload.js
@@ -16,39 +16,48 @@ const { cpuUsage } = require('process');
         } catch (e) {}
 
         me.runUpload = () => {
-            const gridStatusFn = me.comm.file.gridStatus;
-            pkg.readJson(gridStatusFn, (grids) => {
-                let valid = false;
-                for (var o in grids) {
-                    if (grids[o].gridToken === req.body.gridToken) {
-                        valid = true;
-                        break;
-                    }
-                }
-                if (!valid) {
-                    res.send({status:'failure', message : 'authentication failure!'});
-                    return true;
-                }
-                const dirn = req.body.objPath;
-                var cmd = 'mkdir -p ' + dirn;
-                exec(cmd, () =>{
-                    const _f = {};
-                    for (let i = 0; i < req.files.length; i++) {
-                        _f['S_' + i] = ((i)=> {
-                            return (cbk) => {
-                                exec('mv -f ' + req.files[i].path + ' ' + dirn + '/' + req.files[i].originalname, 
-                                    {maxBuffer: 224 * 2048},
-                                    (error, stdout, stderr) =>{
-                                        cbk(true);
-                                    });                  
+            fs.readFile(env.dataFolder  + '/_ip', 'utf-8',
+                function(err, ipData) {
+                    const ip = ipData.replace(/(\n|\r)/ig, '')
+                    const gridStatusFn = me.comm.file.gridStatus;
+                    pkg.readJson(gridStatusFn, (grids) => {
+                        let valid = false;
+                        if (ip === 'localhost') {
+                            valid = true;
+                        } else {
+                            for (var o in grids) {
+                                if (grids[o].gridToken === req.body.gridToken) {
+                                    valid = true;
+                                    break;
+                                }
                             }
-                        })(i)
-                    }
-                    CP.serial(_f, (result) => {
-                        res.send(req.files);
-                    }, 3000);                
-                });
+                        }
+                        if (!valid) {
+                            res.send({status:'failure', message : 'authentication failure!'});
+                            return true;
+                        }
+                        const dirn = req.body.objPath;
+                        var cmd = 'mkdir -p ' + dirn + ' && echo "' + ip + '-' + env.dataFolder  + '/_ip' + '" > ip.txt';
+                        exec(cmd, () =>{
+                            const _f = {};
+                            for (let i = 0; i < req.files.length; i++) {
+                                _f['S_' + i] = ((i)=> {
+                                    return (cbk) => {
+                                        exec('mv -f ' + req.files[i].path + ' ' + dirn + '/' + req.files[i].originalname, 
+                                            {maxBuffer: 224 * 2048},
+                                            (error, stdout, stderr) =>{
+                                                cbk(true);
+                                            });                  
+                                    }
+                                })(i)
+                            }
+                            CP.serial(_f, (result) => {
+                                res.send(req.files);
+                            }, 3000);                
+                        });
+                    });
             });
+
         }
     }
     module.exports = obj;
