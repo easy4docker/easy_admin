03e77565e4f6c93634d1406a9c98c9ba3651c056
easy4docker <easy4docker@gmail.com>
2021-03-03 18:29:47 -0800

    Update moduleServer.js

--- app/cronjob/modules/moduleServer.js
+++ app/cronjob/modules/moduleServer.js
@@ -260,7 +260,7 @@ const { eventNames } = require('process');
                 if (err && err.code === 'ENOENT') {
                     let dirn = folderObj.dir + '/' + folderObj.folder + '/';
                     me.readJson(dirn + '_dockerSetting.json',  (setting) => {
-                        let host = (setting.onDemandCallbackHost === 'localhost') ? 'localhost' : (setting.onDemandCallbackHost + ':10000'); 
+                        let host = (setting.onDemandCallbackHost === 'localhost') ? 'localhost' : (setting.onDemandCallbackHost); 
                         let cmd = 'cd ' + dirn;
 
                         fs.writeFile(me.siteCommCronMark, folderObj.folder, () => {
