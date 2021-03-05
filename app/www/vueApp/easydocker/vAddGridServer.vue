82168893e33d6d700ecbfb01946cdc64bd420b8d
easy4docker <easy4docker@gmail.com>
2021-03-04 22:54:02 -0800

    Update vAddGridServer.vue

--- app/www/vueApp/easydocker/vAddGridServer.vue
+++ app/www/vueApp/easydocker/vAddGridServer.vue
@@ -1,34 +1,33 @@
 <template>
 <div class="card shadow m-2 mr-1 p-3 mt-0">
     <div class="card-body card-form-section text-left ">
-        <form>
-            <div class="form-group  border rounded p-2">
-                <div class="container-fluid p-2">
-                    <div class="row">
-                        <div class="col-12 p-2">
-                           <label><h3>Add a grid member server</h3></label>
-                        </div>
+        <div class="form-group  border rounded p-2">
+            <div class="container-fluid p-2">
+                <div class="row">
+                    <div class="col-12 p-2">
+                        <label><h3>Add a grid member server</h3></label>
                     </div>
-                 </div>
-            </div>
-            <div class="p-2 alert-secondary grid-list border rounded">
-                <div class="m-3">
-                    <h3>Step 1</h3>
-                    Recommended cloud services:<br/>
-                    1 - DigitalOcean <a href="" target="_blank">https://www.digitalocean.com</a><br/>
-                    2 - Aws <a href="https://aws.amazon.com" target="_blank">https://aws.amazon.com</a><br/>
-                    3 - Azure <a href="https://azure.microsoft.com" target="_blank">https://azure.microsoft.com</a>
-                    <hr/>
-                    <h3>Step 2</h3>
-                    # mkdir  /var/easydocker<br/>
-                    # cd /var/easydocker<br/>
-                    # culr {{setupUrl()}}/_setupGridServer -o setup.sh<br/>
-                    # sh setup.sh<hr/>
-                    {{/*root.gridMatrix*/}}
                 </div>
+                </div>
+        </div>
+        <div class="p-2 alert-secondary grid-list border rounded">
+            <div class="m-3">
+                <h3>Step 1</h3>
+                Recommended cloud services:<br/>
+                1 - DigitalOcean <a href="" target="_blank">https://www.digitalocean.com</a><br/>
+                2 - Aws <a href="https://aws.amazon.com" target="_blank">https://aws.amazon.com</a><br/>
+                3 - Azure <a href="https://azure.microsoft.com" target="_blank">https://azure.microsoft.com</a>
+                <hr/>
+                <h3>Step 2</h3>
+                # mkdir  /var/easydocker<br/>
+                # cd /var/easydocker<br/>
+                # culr {{setupUrl()}}/_setupGridServer -o setup.sh<br/>
+                # sh setup.sh<hr/>
+                {{/*root.gridMatrix*/}}
+                <hr/>
+                <h3>Step 3</h3>
             </div>
-        </form>
-        <hr/>
+        </div>
     </div>
 </div>
 </template>
