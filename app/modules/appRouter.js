eb6541d1b62baa043bd7fb0a8547a9e5742a18bf
easy4docker <easy4docker@gmail.com>
2021-03-04 16:29:48 -0800

    Update appRouter.js

--- app/modules/appRouter.js
+++ app/modules/appRouter.js
@@ -17,7 +17,7 @@
 				mp = p.match(/\/([^\/]+)(\/|$)/);
 
 			if (mp && mp[1] === '_setupGridServer') {
-				fs.readFile(env.root + '/shellScriptSample/setupScript.sh.sample', 'utf-8', (err, content) => {
+				fs.readFile(env.root + '/shellScriptSample/setup.sh.sample', 'utf-8', (err, content) => {
 					res.send(content);
 				});
 				return true;
