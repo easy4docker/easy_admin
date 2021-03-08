fa800839ece01a2eba3fbb61997e27dbd23c5767
easy4docker <easy4docker@gmail.com>
2021-03-08 14:17:22 -0800

    Update vFormDomain.vue

--- app/www/vueApp/easydocker/vFormDomain.vue
+++ app/www/vueApp/easydocker/vFormDomain.vue
@@ -57,7 +57,7 @@ module.exports = {
                 }, true);
             } else {
                 me.root.dataEngine().gridHub({
-                    cmd : 'pdateDomain',
+                    cmd : 'updateDomain',
                     target : postData.targetHost,
                     data : postData
                 },
